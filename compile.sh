48375b094eddbdcf85518c202ae235ac0f9ad7cc
burntcarrot <aadhav.n1@gmail.com>
2021-12-09 00:16:06 +0530

    rename to lowercase

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ gcc Algorithms/RLE/rle.c -o Execs/rle
 gcc Algorithms/RLE/efficient-rle.c -o Execs/efficientrle
 gcc Algorithms/FGK/fgkc.c Algorithms/FGK/gtbitio.c Algorithms/FGK/huf.c -o Execs/adaptive-huffman
 gcc Algorithms/LempelZiv/LZ77/lz772.c -o Execs/lz77
-gcc Algorithms/LempelZiv/LZ78/LZWGT.c -o Execs/lz78
+gcc Algorithms/LempelZiv/LZ78/lzwgt.c -o Execs/lz78
