d9d73347b74214a8d23b385ef3f121d777f7e723
burntcarrot <aadhav.n1@gmail.com>
2021-12-09 00:25:35 +0530

    add outputs and fix relative paths

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-gcc Algorithms/Huffman/gthc.c Algorithms/Huffman/gtbitio.c Algorithms/Huffman/huf.c -o Execs/huffman
-gcc Algorithms/RLE/rle.c -o Execs/rle
-gcc Algorithms/RLE/efficient-rle.c -o Execs/efficientrle
-gcc Algorithms/FGK/fgkc.c Algorithms/FGK/gtbitio.c Algorithms/FGK/huf.c -o Execs/adaptive-huffman
-gcc Algorithms/LempelZiv/LZ77/lz772.c -o Execs/lz77
-gcc Algorithms/LempelZiv/LZ78/lzwgt.c -o Execs/lz78
+gcc ./Algorithms/Huffman/gthc.c ./Algorithms/Huffman/gtbitio.c ./Algorithms/Huffman/huf.c -o ./Execs/huffman
+gcc ./Algorithms/RLE/rle.c -o ./Execs/rle
+gcc ./Algorithms/RLE/efficient-rle.c -o ./Execs/efficientrle
+gcc ./Algorithms/FGK/fgkc.c ./Algorithms/FGK/gtbitio.c ./Algorithms/FGK/huf.c -o ./Execs/adaptive-huffman
+gcc ./Algorithms/LempelZiv/LZ77/lz772.c -o ./Execs/lz77
+gcc ./Algorithms/LempelZiv/LZ78/lzwgt.c -o ./Execs/lz78
--- generate_csv.py
+++ generate_csv.py
@@ -6,7 +6,7 @@ folderpath = './Datasets/'
 execs = ["huffman", "adaptive-huffman", "rle", "efficientrle", "lz77", "lz78"]
 
 for ex in execs:
-    with open("csvs/" + ex + ".csv", 'w', newline='') as file:
+    with open("./csvs/" + ex + ".csv", 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Dataset", "Dataset_Path", "Original_Size", "Compressed_Size", "Compression_Ratio"])
 
@@ -15,12 +15,12 @@ for ex in execs:
                 full_path = os.path.join(dirname, fname)
 
                 if ex == "lz78":
-                    os.system("./Execs/" + ex + " -n " + full_path + " Outputs/" + ex + "/" + "outfile-" + fname + ".txt")
+                    os.system("./Execs/" + ex + " -n " + full_path + " ./Outputs/" + ex + "/" + "outfile-" + fname + ".txt")
                 else:
-                    os.system("./Execs/" + ex + " " + full_path + " Outputs/" + ex + "/" + "outfile-" + fname + ".txt")
+                    os.system("./Execs/" + ex + " " + full_path + " ./Outputs/" + ex + "/" + "outfile-" + fname + ".txt")
                 
                 original_size = os.path.getsize(str(full_path))
-                compressed_size = os.path.getsize("Outputs/" + ex + "/" + "outfile-" + fname + ".txt")
+                compressed_size = os.path.getsize("./Outputs/" + ex + "/" + "outfile-" + fname + ".txt")
                 
                 print(f"Writing {fname} stats to CSV:")
                 writer.writerow([fname, full_path, original_size, compressed_size, (1 - compressed_size/original_size)])
