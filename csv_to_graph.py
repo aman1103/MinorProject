887afcdc8c19c3afc9ab6612bf6c6b195262fb86
burntcarrot <aadhav.n1@gmail.com>
2021-12-09 19:46:58 +0530

    update bar graph palette and font size

--- csv_to_graph.py
+++ csv_to_graph.py
@@ -32,7 +32,9 @@ plotd['Dataset'] = datasets[0][:26]
 
 for i in range(5):
     plotd_new = plotd.iloc[(i*5):((i+1)*5)]
-    ax = plotd_new.plot.bar(x = "Dataset", rot=0, figsize=(20, 15), fontsize=18)
+    ax = plotd_new.plot.bar(x = "Dataset", rot=0, figsize=(20, 15), fontsize=18, color=['#FEC606', '#D33257', '#1DABB8', '#2CC990', '#E22211', '#11156D'])
     ax.legend(fontsize=16)
+    ax.set_xlabel('Dataset', fontsize = 24)
+    ax.set_ylabel('Compression Ratio (in percentage)', fontsize = 24)
     fig = ax.get_figure()
     fig.savefig("./Results/" + "output" + str(i+1) + ".png")
